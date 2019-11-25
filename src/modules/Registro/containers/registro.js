97608a15b07500e2d2c2cdebc3a1a66c820a6f9b
Oscar Reyes <ooreyes.ro@gmail.com>
2019-11-25 17:47:29 -0300

    mejora en registro
    falta agregar el touchable a registro

--- src/modules/Registro/containers/registro.js
+++ src/modules/Registro/containers/registro.js
@@ -1,50 +1,82 @@
 ﻿import React, { Component } from 'react';
 import { StyleSheet } from 'react-native'
-import { Container, Input, Label, Item, Content, Card, CardItem, Text, Body, Button } from 'native-base';
+import { Container, Input, Label, Item, Content, Card, CardItem, Text, Body, Button, Form, View, TouchableHighlight } from 'native-base';
 
-export default class AnatomyExample extends Component {
+
+export default class Registro extends Component {
     render() {
         return (
-            <Container>
 
-                <Content padder contentContainerStyle={styles.content}>
-                    <Card>
-                        <CardItem header bordered>
-                            <Text style={styles.textCenter}>de prueba </Text>
-                        </CardItem>
-                        <CardItem bordered>
-                            <Body>
-                                <Item rounded>
-                                    <Input placeholder='Usuario' />
-                                </Item>
-                                <Item floatingLabel last>
-                                    <Label>Contraseña</Label>
-                                    <Input />
-                                </Item>
-                            </Body>
-                        </CardItem>
-                        <CardItem footer bordered>
-                            <Button primary style={styles.boton}><Text> Iniciar </Text></Button>
-                        </CardItem>
+            <View style={[styles.container]} >
+                <Container>
+                    <Content>
+                        <Form style={[styles.form]}>
+                            <Item floatingLabel>
+                                <Label>Nombre</Label>
+                                <Input />
+                            </Item>
+                            <Item floatingLabel last>
+                                <Label>Correo</Label>
+                                <Input />
+                            </Item>
+                            <Item floatingLabel>
+                                <Label>Contraseña</Label>
+                                <Input />
+                            </Item>
+                            <Item floatingLabel last>
+                                <Label>Confirma la contraseña</Label>
+                                <Input />
+                            </Item>
+                            
+                        </Form>
+                        <Button primary style={styles.boton}><Text> Registrarse </Text></Button>
+                    </Content>
+                </Container>
+            </View>
 
-                    </Card>
-                </Content>
-            </Container>
+
         );
     }
 }
 
 const styles = StyleSheet.create({
-    textCenter: {
-        textAlign: 'center',
-        width: '100%'
-    },
-    content: {
+    container: {
         flex: 1,
-        justifyContent: 'center'
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#E27319',
+        
     },
-    boton: {
-        marginLeft: '40%'
-    }
+    form: {
+        flex: 1,  
+        justifyContent: 'center',
+        resizeMode: 'center',
 
+    },
+    image: {
+        width: 300,
+        height: 300,
+        borderRadius: 150,
+        resizeMode: 'center'
+    },
+    headerLeft: {
+        flex: 1
+
+    },
+    texto: {
+        color: '#E27319',
+        fontSize: 18,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        width: 200,
+        marginTop: 10,
+        marginLeft: 10,
+    },
+    button: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#DDDDDD',
+        padding: 10,
+    }
 })
